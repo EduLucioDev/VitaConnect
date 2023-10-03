22bff4a7adf69dde34e5a85e5f7b3eb155e1ebd4
EduLucioDev <duio.el@gmail.com>
2023-10-03 07:46:56 -0300

    Criação local das tabelas Paciente e EstadoDePaciente. Configurações de conexão do db.

--- ProjetoCurso/VitaConnect.Demo/src/VitaConnect.ItDeveloper.Mvc/Program.cs
+++ ProjetoCurso/VitaConnect.Demo/src/VitaConnect.ItDeveloper.Mvc/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using VitaConnect.ItDeveloper.Data.Data.ORM;
+
 namespace VitaConnect.ItDeveloper.Mvc
 {
     public class Program
@@ -8,6 +11,9 @@ namespace VitaConnect.ItDeveloper.Mvc
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
