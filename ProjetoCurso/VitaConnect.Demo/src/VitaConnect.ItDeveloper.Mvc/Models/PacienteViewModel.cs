05c88f66e83023bfff2e388bcc926ecf27956aab
EduLucioDev <duio.el@gmail.com>
2023-10-03 20:41:31 -0300

    Annotations do PacienteViewModel e EstadoPacienteViewModel

--- ProjetoCurso/VitaConnect.Demo/src/VitaConnect.ItDeveloper.Mvc/Models/PacienteViewModel.cs
+++ ProjetoCurso/VitaConnect.Demo/src/VitaConnect.ItDeveloper.Mvc/Models/PacienteViewModel.cs
@@ -9,29 +9,87 @@ namespace VitaConnect.ItDeveloper.Mvc.Models
 	public class PacienteViewModel
 	{
 		[Key]
-		[DisplayName(displayName: "Id do Paciente")]
-		public string Id { get; set; }
+		public Guid Id { get; set; }
 
+		[ForeignKey("EstadoPaciente")]
+		[Display(Name = "Estado do Paciente")]
 		public Guid EstadoPacienteId { get; set; }
-		public virtual EstadoPacienteViewModel? EstadoPaciente { get; set; }
+		public virtual EstadoPaciente? EstadoPaciente { get; set; }
 
+
+		[DisplayName(displayName: "Nome do Paciente")]
+		[StringLength(maximumLength: 80, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres.", MinimumLength = 2)]
 		public string? Nome { get; set; }
-		[Display(Name = "Data Nascimento")]
-		[DataType(DataType.DateTime, ErrorMessage = "O campo {0} está inválido")]
+
+
+		[DisplayName(displayName: "Data Nascimento")]
+		[Required(ErrorMessage = "Campo {0} é requerido.")]
+		[DataType(DataType.DateTime, ErrorMessage = "Data Inválida.")]
 		public DateTime DataNascimento { get; set; }
-		[Display(Name = "Data de Internação")]
-		[DataType(DataType.DateTime, ErrorMessage = "O campo {0} está inválido")]
+
+
+		[DisplayName(displayName: "Data Internação")]
+		[Required(ErrorMessage = "Campo {0} é requerido.")]
+		[DataType(DataType.DateTime, ErrorMessage = "Data Inválida.")]
 		public DateTime DataInternacao { get; set; }
+
+
+
+		//[DisplayName(displayName: "Data Inclusão")]
+		//public DateTime? DataInclusao { get; set; }
+
+		//[DisplayName(displayName: "Data Última Modificação")]
+		//public DateTime? DataUltimaModificacao { get; set; }
+
+		//[DisplayName(displayName: "Usuário Inclusão")]
+		//public string? UsuarioInclusao { get; set; }
+
+		//[DisplayName(displayName: "Usuário Última Modificação")]
+		//public string? UsuarioUltimaModificacao { get; set; }
+
+
+
+		[DisplayName(displayName: "Email")]
+		[DataType(DataType.EmailAddress, ErrorMessage = "Email Inválido.")]
 		public string? Email { get; set; }
+
 		public bool Ativo { get; set; }
+
+
+		[DisplayName(displayName: "CPF")]
+		[Required(ErrorMessage = "Campo {0} é requerido.")]
+		[StringLength(11, ErrorMessage = "Campo {0} tem de ter {1} caracteres", MinimumLength = 11)]
 		public string? Cpf { get; set; }
+
+		[DisplayName(displayName: "Tipo de Paciente")]
+		[Required(ErrorMessage = "O campo {0} é requerido.")]
 		public TipoPaciente TipoPaciente { get; set; }
+
+		[DisplayName(displayName: "Sexo")]
+		[Required(ErrorMessage = "O campo {0} é requerido.")]
 		public Sexo Sexo { get; set; }
+
+		[DisplayName(displayName: "RG")]
+		[MaxLength(15, ErrorMessage = "O campo {0} não pode ter mais que (1) caracteres.")]
 		public string? Rg { get; set; }
+
+		[DisplayName(displayName: "Org.Expedidor")]
+		[MaxLength(10, ErrorMessage = "O campo {0} não pode ter mais que (1) caracteres.")]
 		public string? RgOrgao { get; set; }
-		[Display(Name = "Data de emissão do RG")]
-		[DataType(DataType.DateTime, ErrorMessage = "O campo {0} está inválido")]
+
+		[DisplayName(displayName: "Data Emissão")]
+		[Required(ErrorMessage = "Campo {0} é requerido.")]
+		[DataType(DataType.DateTime, ErrorMessage = "Data Inválida.")]
 		public DateTime RgDataEmissao { get; set; }
+
+
+		[MaxLength(90, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres.")]
 		public string? Motivo { get; set; }
+
+
+		public override string ToString()
+		{
+			return this.Id.ToString() + "  -  " + this.Nome;
+		}
 	}
 }
