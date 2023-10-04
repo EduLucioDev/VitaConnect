7456e346a09563bd75b30734ff3ac9ec4a8030b9
EduLucioDev <duio.el@gmail.com>
2023-10-04 07:52:36 -0300

    Mapping EstadoPacienteMap

---
+++ ProjetoCurso/VitaConnect.Demo/src/VitaConnect.ItDeveloper.Data/Mappings/EstadoPacienteMap.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VitaConnect.ItDeveloper.Domain.Entities;
+
+namespace VitaConnect.ItDeveloper.Data.Mappings
+{
+    public class EstdoPacienteMap : IEntityTypeConfiguration<EstadoPaciente>
+    {
+        public void Configure(EntityTypeBuilder<EstadoPaciente> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Descricao).HasColumnType("varchar(30)")
+                .IsRequired().HasColumnName("Descricao");
+            builder.HasMany(p => p.Pacientes)
+                .WithOne(p => p.EstadoPaciente);
+                //.OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
